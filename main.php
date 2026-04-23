ad5fe48ca086fc9198134678dd8007cbba677550
lucas-01script01 <lucas.r.freitas6@edu.senai.br>
2026-04-23 11:45:17 -0300

    v.0.2 - acescentado funçaos.

--- main.php
+++ main.php
@@ -1,2 +1,12 @@
 <?php
 echo "na aula no dia 23/04/2026 eu aprendi a meer no github/git e desenvolver funçoes no php! =D";
+function somar ($a, $b, $c) {
+    return $a + $b + $c;
+
+}
+echo "resultado ", somar (10,5,3);
+function subtrair( $a , $b){
+    return $a - $b;
+    
+}
+echo "resultado ", subtrair ( 10 , 5 );
